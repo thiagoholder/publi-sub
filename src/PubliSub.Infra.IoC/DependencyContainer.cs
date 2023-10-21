34812d8c75953e32d1b6b643eb4de7e191a591df
Thiago Holder <thiagoholder@gmail.com>
2023-10-21 13:51:00 +0100

    Implemented Dependency Container in Infra IoC

---
+++ src/PubliSub.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PubliSub.Domain.Core.Bus;
+using PubliSub.Infra.Bus;
+
+namespace PubliSub.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
